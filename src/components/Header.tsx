7415af10ed1c439666e8129587b5bc60cce03b44
kaori <kaori@weseek.co.jp>
2021-01-17 23:09:43 +0900

    importing changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Link from 'next/link';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import { AppBar, Toolbar, Typography, IconButton, MenuItem, Menu } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
+import { AccountCircle } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
